0be79ea4dafee842b4f255257c52c780668c22a3
caioseguchi <caioseguchi@gmail.com>
2026-06-20 12:07:19 +0200

    URL configuration 2

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,10 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+import { defineConfig } from "astro/config";
 
 const posts = defineCollection({
-  loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
+  loader: glob({ base: "./src/content/posts", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),
@@ -15,7 +16,7 @@ const posts = defineCollection({
 });
 
 const pages = defineCollection({
-  loader: glob({ base: './src/content/pages', pattern: '**/*.{md,mdx}' }),
+  loader: glob({ base: "./src/content/pages", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
     date: z.coerce.date().optional(),
@@ -26,4 +27,6 @@ const pages = defineCollection({
 export const collections = { posts, pages };
 
 export default defineConfig({
-  site: ''
+  site: "https://caioseguchi.github.io",
+  // SEM "base", pois o repo já é username.github.io
+});
